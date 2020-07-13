634d5d102b48ead9d7e5b5ffa8470e093360c051
Chester <chesterdeveloper2000@gmail.com>
2020-07-13 12:38:02 +0800

    installed caporal, learned about the 3 types of variable naming conventions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
-const debounce = require("lodash.debounce")
-const chokidar = require("chokidar")
+const debounce = require("lodash.debounce") // named debounce instead of lodashDebounce since lodash has way more functionalities than debounce
+const chokidar = require("chokidar") //named chokidar since it's a simple program
+const program = require("caporal") //named program because the documentation says so
 
 const start = debounce(() => {
     console.log("Starting user's program")
